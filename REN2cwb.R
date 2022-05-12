83655f1a98661959ab2334d1b6028bdc9c1dbc58
Stefan Hartmann <stefanhartmann@Stefan-MBP.fritz.box>
2022-05-12 14:47:54 +0200

    initial commit

---
+++ REN2cwb.R
@@ -0,0 +1,255 @@
+library(tidyverse)
+
+# list files
+f <- list.files("ReN_anno_2019-08-14/", full.names = T)
+
+for(j in 1:length(f)) {
+  
+  # read data
+  d <- readLines(f[j])
+  
+  # get metadata
+  header_name <- gsub("\".*", "", gsub(".*name=\"", "", grep("<cora-header name", d, value = T)))
+  
+  # get header
+  header_start <- grep("<header", d)
+  header_end   <- grep("</header", d)
+  hdr <- d[header_start:header_end]
+  
+  
+  
+  hdr <- sub(":", "=\"", hdr)
+  hdr <- gsub("(?<=$)", "\"", hdr, perl = T)
+  text_header <- paste0("<text id=\"", header_name, "\"", gsub("text=\"", "text_name=\"", gsub("</?header>", "", paste0(hdr, collapse = " "))), ">", collapse = " ")
+  
+  # get tokens
+  token_start <- grep("<token id", d)
+  token_end   <- grep("</token", d)
+  
+  # empty table for dipl, lemma, POS, morph
+  cur_tbl <- tibble(
+    word      = character(length(token_start)),
+    token_id  = character(length(token_start)),
+    dipl      = character(length(token_start)),
+    lemma     = character(length(token_start)),
+    pos       = character(length(token_start)),
+    morph     = character(length(token_start)),
+    lemma_wsd = character(length(token_start)),
+    bound_sent = character(length(token_start)),
+  )
+  
+  
+  # extract values
+  
+  for(i in 1:length(token_start)) {
+    cur <-d[token_start[i]:token_end[i]]
+    
+    if(length(grep(".*<token id=\"", cur)) > 0) {
+      cur_tbl$token_id[i] <- gsub("\".*", "", gsub(".*<token id=\"", "", cur[grep("token id", cur)]))
+    } else {
+      cur_tbl$token_id[i] <- "-"
+    }
+    
+    if(length(grep(".*<token id=\".*trans", cur)) > 0) {
+      cur_tbl$dipl[i] <- gsub("\".*", "", gsub(".*trans=\"", "", cur[grep("token id", cur)]))
+    } else {
+      cur_tbl$dipl[i] <- "-"
+    }
+    
+    if(length(grep(".*<token id=\".*trans", cur)) > 0) {
+      cur_tbl$word[i] <- gsub("\".*", "", gsub(".*trans=\"", "", cur[grep("token id", cur)]))
+    } else {
+      cur_tbl$word[i] <- "-"
+    }
+    
+    if(length(grep(".*<lemma tag=\"", cur)) > 0) {
+      cur_tbl$lemma[i] <- gsub("\".*", "", gsub(".*<lemma tag=\"", "", cur[grep("lemma tag", cur)]))
+    } else {
+      cur_tbl$lemma[i] <- "-"
+    }
+    
+    if(length(grep(".*<lemma_wsd tag=\"", cur)) > 0) {
+      cur_tbl$lemma_wsd[i] <- gsub("\".*", "", gsub(".*<lemma_wsd tag=\"", "", cur[grep("lemma_wsd tag", cur)]))
+    } else {
+      cur_tbl$lemma_wsd[i] <- "-"
+    }
+    
+    if(length(grep(".*<morph tag=\"", cur)) > 0) {
+      cur_tbl$morph[i] <- gsub("\".*", "", gsub(".*<morph tag=\"", "", cur[grep("morph tag", cur)]))
+    } else {
+      cur_tbl$morph[i] <- "-"
+    }
+    
+    if(length(grep(".*<pos tag=\"", cur)) > 0) {
+      cur_tbl$pos[i] <- gsub("\".*", "", gsub(".*<pos tag=\"", "", cur[grep("pos tag", cur)]))
+    } else {
+      cur_tbl$pos[i] <- "-"
+    }
+    
+    if(length(grep(".*<bound_sent tag=\"", cur)) > 0) {
+      cur_tbl$bound_sent[i] <- gsub("\".*", "", gsub(".*<bound_sent tag=\"", "", cur[grep("bound_sent tag", cur)]))
+    } else {
+      cur_tbl$bound_sent[i] <- "-"
+    }
+    
+    
+    
+  }
+  
+  
+  # create CWB file
+  
+  # file header
+  
+  if(j == 1) {
+    write.table(c("<?xml version=\"1.0\" encoding=\"UTF-8\"?>", "<corpus>"),
+                "ren.vrt", row.names = F, col.names = F, quote = F)
+  }
+  
+  write.table(text_header,
+              "ren.vrt", row.names = F, col.names = F, quote = F, append = T)
+  write.table(cur_tbl,
+              "ren.vrt", sep = "\t", row.names = F, col.names = F, quote = F, append = T)
+  write.table("</text>",
+              "ren.vrt", row.names = F, col.names = F, quote = F, append = T)
+  
+  if(j == length(f)) {
+    write.table("</corpus>",
+                "ren.vrt", row.names = F, col.names = F, quote = F, append = T)
+  }
+  
+  print(j)
+  
+}
+
+
+# 
+# 
+# # read data
+# d <- readLines(f[1])
+# 
+# # get metadata
+# header_name <- gsub("\".*", "", gsub(".*name=\"", "", grep("<cora-header name", d, value = T)))
+# 
+# # get header
+# header_start <- grep("<header", d)
+# header_end   <- grep("</header", d)
+# hdr <- d[header_start:header_end]
+# 
+# 
+# 
+# hdr <- sub(":", "=\"", hdr)
+# hdr <- gsub("(?<=$)", "\"", hdr, perl = T)
+# text_header <- paste0("<text id=\"", header_name, "\"", gsub("text=\"", "text_name=\"", gsub("</?header>", "", paste0(hdr, collapse = " "))), ">", collapse = " ")
+# 
+# # get tokens
+# token_start <- grep("<token id", d)
+# token_end   <- grep("</token", d)
+# 
+# # empty table for dipl, lemma, POS, morph
+# cur_tbl <- tibble(
+#   word     = character(length(token_start)),
+#   token_id = character(length(token_start)),
+#   dipl     = character(length(token_start)),
+#   lemma    = character(length(token_start)),
+#   pos      = character(length(token_start)),
+#   morph    = character(length(token_start))
+# )
+# 
+# 
+# # extract values
+# 
+# for(i in 1:length(token_start)) {
+#   cur <-d[token_start[i]:token_end[i]]
+#   
+#   if(length(grep(".*<token id=\"", cur)) > 0) {
+#     cur_tbl$token_id[i] <- gsub("\".*", "", gsub(".*<token id=\"", "", cur[grep("token id", cur)]))
+#   } else {
+#     cur_tbl$token_id[i] <- "-"
+#   }
+#   
+#   if(length(grep(".*<token id=\".*trans", cur)) > 0) {
+#     cur_tbl$dipl[i] <- gsub("\".*", "", gsub(".*trans=\"", "", cur[grep("token id", cur)]))
+#   } else {
+#     cur_tbl$dipl[i] <- "-"
+#   }
+#   
+#   if(length(grep(".*<token id=\".*trans", cur)) > 0) {
+#     cur_tbl$word[i] <- gsub("\".*", "", gsub(".*trans=\"", "", cur[grep("token id", cur)]))
+#   } else {
+#     cur_tbl$word[i] <- "-"
+#   }
+#   
+#   if(length(grep(".*<lemma tag=\"", cur)) > 0) {
+#     cur_tbl$lemma[i] <- gsub("\".*", "", gsub(".*<lemma tag=\"", "", cur[grep("lemma tag", cur)]))
+#   } else {
+#     cur_tbl$lemma[i] <- "-"
+#   }
+#   
+#   if(length(grep(".*<morph tag=\"", cur)) > 0) {
+#     cur_tbl$morph[i] <- gsub("\".*", "", gsub(".*<morph tag=\"", "", cur[grep("morph tag", cur)]))
+#   } else {
+#     cur_tbl$morph[i] <- "-"
+#   }
+#   
+#   if(length(grep(".*<pos tag=\"", cur)) > 0) {
+#     cur_tbl$pos[i] <- gsub("\".*", "", gsub(".*<pos tag=\"", "", cur[grep("pos tag", cur)]))
+#   } else {
+#     cur_tbl$pos[i] <- "-"
+#   }
+#   
+#   
+#   
+# }
+# 
+# 
+# # create CWB file
+# 
+# 
+# 
+# # file header
+# write.table(c("<?xml version=\"1.0\" encoding=\"UTF-8\"?>", "<corpus>"),
+#             "ren.vrt", row.names = F, col.names = F, quote = F)
+# write.table(text_header,
+#             "ren.vrt", row.names = F, col.names = F, quote = F, append = T)
+# write.table(cur_tbl,
+#             "ren.vrt", sep = "\t", row.names = F, col.names = F, quote = F, append = T)
+# write.table(c("</text>", "</corpus>"),
+#             "ren.vrt", row.names = F, col.names = F, quote = F, append = T)
+# 
+# 
+# 
+# 
+# cur <-d[token_start[1]:token_end[1]]
+# 
+# if(length(grep(".*<token id=\"", cur)) > 0) {
+#   cur_tbl$token_id[i] <- gsub("\".*", "", gsub(".*<token id=\"", "", cur[grep("token id", cur)]))
+# } else {
+#   cur_tbl$token_id[i] <- "-"
+# }
+# 
+# if(length(grep(".*<token id=\".*trans", cur)) > 0) {
+#   cur_tbl$dipl[i] <- gsub("\".*", "", gsub(".*trans=\"", "", cur[grep("token id", cur)]))
+# } else {
+#   cur_tbl$dipl[i] <- "-"
+# }
+# 
+# if(length(grep(".*<lemma tag=\"", cur)) > 0) {
+#   cur_tbl$lemma[i] <- gsub("\".*", "", gsub(".*<lemma tag=\"", "", cur[grep("lemma tag", cur)]))
+# } else {
+#   cur_tbl$lemma[i] <- "-"
+# }
+# 
+# if(length(grep(".*<morph tag=\"", cur)) > 0) {
+#   cur_tbl$morph[i] <- gsub("\".*", "", gsub(".*<morph tag=\"", "", cur[grep("morph tag", cur)]))
+# } else {
+#   cur_tbl$morph[i] <- "-"
+# }
+# 
+# if(length(grep(".*<pos tag=\"", cur)) > 0) {
+#   cur_tbl$morph[i] <- gsub("\".*", "", gsub(".*<pos tag=\"", "", cur[grep("pos tag", cur)]))
+# } else {
+#   cur_tbl$morph[i] <- "-"
+# }
+# 
+# 
---
+++ readme.md
@@ -0,0 +1,12 @@
+# REN2CWB
+
+This repository contains an R script for converting the [Reference Corpus Middle Low German/Low Rhenish](https://corpora.uni-hamburg.de/hzsk/de/islandora/object/text-corpus:ren-1.0#corpus-description) (REN-Team 2019; Schröder 2014, Peters & Nagel 2014) to [CWB](https://cwb.sourceforge.io/). The file cwb-encode.txt also contains the commands for encoding the resulting .vrt file for use in the Corpus Workbench. The REN corpus files are re-used and distributed under the [CC-BY 4.0](https://creativecommons.org/licenses/by/4.0/) license.
+
+
+## References
+
+ReN-Team. 2019. “Referenzkorpus Mittelniederdeutsch/Niederrheinisch (1200-1650).” Archived in Hamburger Zentrum für Sprachkorpora. Version 1.0. Publication date 2019-08-14. http://hdl.handle.net/11022/0000-0007-D829-8.
+
+Ingrid Schröder. 2014. “Das Referenzkorpus: Neue Perspektiven für die mittelniederdeutsche Grammatikographie.” Jahrbuch für Germanistische Sprachgeschichte, 5(1), pp. 150-164. http://www.degruyter.com/view/j/jbgsg.2014.5.issue-1/jbgsg-2014-0011/jbgsg-2014-0011.xml .
+
+Robert Peters, Norbert Nagel. 2014. “Das digitale ‚Referenzkorpus Mittelniederdeutsch / Niederrheinisch (ReN)‘.” Jahrbuch für Germanistische Sprachgeschichte, 5(1), pp. 165-175. https://www.degruyter.com/view/j/jbgsg.2014.5.issue-1/jbgsg-2014-0012/jbgsg-2014-0012.xml .
